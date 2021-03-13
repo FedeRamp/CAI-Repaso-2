4f0e9612ed1e4d9948061f5a723b1243b27c58d4
Federico Rampazzo <federamp91@gmail.com>
2021-03-13 13:01:43 -0300

    Creation

---
+++ Repaso2/Repaso2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repaso2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
